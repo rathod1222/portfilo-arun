0a31674a4e729e5168b965d618aff2327ea7fae8
rathod122 <arunrathod0706@gmail.com>
2024-09-29 23:19:11 +0530

    Your commit message

---
+++ src/data/constants.js
@@ -0,0 +1,164 @@
+export const Bio = {
+  name: "Arun Rathod",
+  roles: ["DevOps Engineer"],
+  description: `A Professional having 3+ years of IT experience software industry in DevOps and AWS Cloud using various
+CI/CD tools such as GIT, MAVEN, JENKINS, ANSIBLE, DOCKER, KUBERNETES, TERRAFORM and AWS Cloud
+Services, looking for an opportunity where I can enhance my skills to a good level and hence can grow more in
+techno management side`,
+  github: "https://github.com/rathod1222",
+  resume:
+    "https://drive.google.com/file/d/1rJ9xcIzfdlen7vveTaBus9kiHXPKfeFM/view?usp=drivesdk",
+};
+
+export const skills = [
+  {
+    title: "AWS Cloud",
+    skills: [
+      {
+        name: "Jenkins",
+        image:
+          "https://logowik.com/content/uploads/images/jenkins8460.jpg",
+      },
+      {
+        name: "TerraForm",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQaVUkTuGBq3yswYjlP7QtAPMEDV3Fzj5myTr2TKXinKEUfL1NlxRK7KRbpZezrL8wkRs&usqp=CAU",
+      },
+      {
+        name: "Docker",
+        image:
+"https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/97_Docker_logo_logos-512.png"      },
+
+      {
+        name: "Kubernetes",
+        image:
+"https://upload.wikimedia.org/wikipedia/commons/3/39/Kubernetes_logo_without_workmark.svg"      },
+
+      {
+        name: "Linux",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQyh-zB2dHv9xdoULi_xFlXoq3bHkJCPh8ug&s",
+      },
+      {
+        name: "Git",
+        image:
+"https://book.git-scm.com/images/logos/downloads/Git-Icon-1788C.png"      },
+      {
+        name: "Ansible",
+        image:
+          "https://logowik.com/content/uploads/images/ansible3554.jpg",
+      },
+      {
+        name: "S3 Bucket",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSodRB4Zwu9gtBmJcf06Iq8SgXfZnGIE7YeKZFb4sG6l_LndqiUxjEn2YPD_l77VAPmvy4&usqp=CAU",
+      },
+      {
+        name: "Aws Cloud ",
+        image:
+          "https://logos-world.net/wp-content/uploads/2021/08/Amazon-Web-Services-AWS-Logo.png",
+      },
+    ],
+  },
+  //
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://softtechcloud.com/wp-content/uploads/2023/02/cropped-IMG-20230221-WA0010.jpg",
+    role: "DevOps Engineer",
+    company: "SoftTech Cloud Technologies pvt ltd ",
+    date: "Aug-2021 - present",
+    desc: "DevOps is all about combining software development (Dev) and IT operations (Ops) to shorten the development lifecycle and deliver high-quality software continuously. Here are the key components and tools that play a significant role in DevOps",
+    skills: [
+      `Version Control: Tools like Git and GitHub help manage code versions and enable collaboration among team members.
+
+Continuous Integration/Continuous Delivery (CI/CD): Tools like Jenkins, GitLab CI, or CircleCI help automate the build, test, and deployment process, ensuring faster delivery and feedback loops.
+
+Infrastructure as Code (IaC): Tools like Terraform and Ansible are used to provision and manage infrastructure using code, allowing environments to be easily replicated and maintained.
+
+Containers: Docker is commonly used to create lightweight, portable containers that package applications and their dependencies, making them easy to deploy across various environments.
+
+Orchestration: Kubernetes is widely used for orchestrating containers, ensuring scalability, availability, and self-healing capabilities of applications.
+
+Monitoring and Logging: Tools like Prometheus, Grafana, ELK Stack (Elasticsearch, Logstash, Kibana) are used for monitoring applications and infrastructure, collecting logs, and visualizing system performance.
+
+Configuration Management: Chef, Puppet, or Ansible help manage configurations across servers and ensure they are consistent and compliant.
+
+Cloud: Platforms like AWS, Azure, or Google Cloud provide scalable infrastructure and services to host and manage applications.`,
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://www.mguindia.com/images/mansarovar-logo.jpg",
+    school: "MANSAROVAR GLOBAL UNIVERSITY, BHOPAL",
+    date: "2018 to 2021",
+    grade: "7.75 CGPA",
+    degree: "Bsc ",
+  },
+  //
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Restaurant",
+    date: "oct 2022 - jan 2023",
+    description:
+      "Start by setting up your React.js project using tools like Create React App or by configuring Webpack and Babel manually. This step involves creating the basic folder structure and installing necessary dependencies",
+    image:
+      "https://t3.ftcdn.net/jpg/03/24/73/92/360_F_324739203_keeq8udvv0P2h1MLYJ0GLSlTBagoXS48.jpg",
+    tags: ["React js", "BootStrap", "MaterialUi", "HTML", "CSS", "Node Js"],
+    github: "https://github.com/rathod1222",
+  },
+  {
+    id: 9,
+    title: "E- Commerce",
+    date: "Feb 2023 - May 2023",
+    description: `Create a dynamic e-commerce project using React.js, integrating essential 
+        features like product listings, shopping cart functionality, 
+        user authentication, and checkout process. Employ modern UI 
+        components, state management libraries like Redux or Context 
+        API, and backend services like Firebase or Express.js for 
+        seamless data management and user experience. 
+        Customize the project to include advanced functionalities such as product search, 
+        sorting, and filtering options to enhance usability and scalability.`,
+    image:
+      "https://assets-global.website-files.com/614d610a3dd0725153e51ded/6222149bfbdb5b0645ae4c0e_eCommerce-Website-Components-photo.jpeg",
+    tags: ["React js", "BootStrap", "MaterialUi", "HTML", "CSS", "Node Js"],
+
+    github: "https://github.com/rathod1222",
+  },
+  {
+    id: 9,
+    title: "Dating Stie App",
+    date: "Dec 2023 - Prasent 2024",
+    description: `1.Developed the frontend user interface for the Dating Site application using React
+        Native.
+        2.Utilized React Native framework to build a cross-platform mobile application.
+        3.Implemented attractive and user-friendly UI designs to enhance user experience.
+        4. Integrated React Native Paper and Tailwind CSS libraries to streamline UI
+        development and ensure consistency across screens.
+        5.Collaborated with the design team to translate mockups and wireframes into
+        interactive UI components.
+        6.Ensured responsiveness and compatibility across various devices and screen
+        sizes.
+        7.Conducted code reviews and collaborated with backend developers to integrate
+        frontend with backend services.
+        8.Implemented navigation and routing functionalities for seamless user flow within
+        the application.
+        `,
+    image:
+      "https://www.lovepanky.com/wp-content/uploads/2015/05/I-Used-a-Dating-Site-Without-a-Photo.jpg",
+    tags: [
+      "React Native ",
+      "React Native Paper",
+      "Tailwind",
+      "CSS",
+      "Android Emulator",
+    ],
+    github: "https://github.com/rathod1222",
+  },
+];
